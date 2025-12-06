e78947c7530045b54114b273b7eb5810249b2e0c
blaze-the-star <startheblaze@gmail.com>
2025-12-06 00:09:01 -0600

    Initial

---
+++ about.md
@@ -0,0 +1,1 @@
+This is the _about_ page.
---
+++ static/libs/process.js
@@ -0,0 +1,42 @@
+
+window.onload = function() {
+	const addNav = document.querySelector('script[nav]');
+	if (!addNav) {
+		return;
+	}
+
+	let header = document.querySelector('header');
+	let footer = document.querySelector('footer');
+  	
+	let prevLink = ``;
+	if (addNav.hasAttribute("prev")) {
+		let prev = addNav.getAttribute('prev')
+		let prevUrl = `/Revival/${prev}`.replace("/Revival//", "/Revival/");
+		prevLink = `<a href='${prevUrl}'>&lt;&lt; Prev</a>`;
+	}
+
+	let nextLink = ``;
+	if (addNav.hasAttribute("next")) {
+		let next = addNav.getAttribute('next')
+		let nextUrl = `/Revival/${next}`.replace("/Revival//", "/Revival/");
+		nextLink = `<a href='${nextUrl}'>Next &gt;&gt;</a>`;
+	}
+
+	let links = prevLink
+		+ `<a href='/Revival/1/1'>First</a>`
+		+ nextLink
+	let nav = `<hr><center>${links}</center><hr>`;
+	
+	if (header) {
+		const newElement = document.createElement('p');
+		newElement.innerHTML = nav;
+		header.appendChild(newElement);
+	}
+
+	if (footer) {
+		const newElement2 = document.createElement('p');
+		newElement2.innerHTML = nav;
+		footer.prepend(newElement2);
+	}
+
+};
